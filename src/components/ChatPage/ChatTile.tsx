7d39210dd1f7c1f74038b7692c2c5b391326d1c3
hodame <hodame04@gmail.com>
2023-07-16 23:20:13 +0500

    create chat tile with info about chat and last message ect

---
+++ src/components/ChatPage/ChatTile.tsx
@@ -0,0 +1,40 @@
+import { Avatar } from "@chakra-ui/react";
+
+type ChatTileProps = {
+  avatarLink: string;
+  userName: string;
+  lastMessage: string;
+  messageTime: string;
+  isRead?: boolean;
+  messageCount: number
+};
+
+export default function ChatTile({ avatarLink, userName, lastMessage, messageTime, isRead, messageCount}: ChatTileProps) {
+  return (
+    <div className="px-3 py-3 rounded-3xl flex justify-between cursor-pointer hover:bg-surface">
+      <Avatar
+        name={userName}
+        size="lg"
+        src={avatarLink}
+      />
+      <div className=" mx-2 flex-auto">
+        <h1 className="text-white font-medium">{userName}</h1>
+        <p className="line-clamp-2 text-sm text-light">
+          {lastMessage}
+        </p>
+      </div>
+      <div className="flex flex-col text-sm items-end">
+        <h1 className={"text-sm text-light"}>{messageTime}</h1>
+        {MessageCount({ count: messageCount })}
+      </div>
+    </div>
+  );
+}
+
+function MessageCount({ count }: { count: number }) {
+  return (
+    <div className="bg-primary inline my-1 rounded-full w-7 h-7 flex items-center justify-center">
+      <span className="text-white font-medium">{count}</span>
+    </div>
+  );
+}
