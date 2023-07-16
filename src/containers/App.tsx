37136b0e4c4c6bc767f1b1de5a25749b60aa2a7a
hodame <hodame04@gmail.com>
2023-07-16 23:22:24 +0500

    add outlet provider and check for current location

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,16 +1,21 @@
-import { Outlet, RouterProvider } from "react-router-dom";
-import SideBar from "../components/SideBar";
+import { Outlet, useLocation } from "react-router-dom";
+import SideBar from "../components/SideBar/SideBar";
 
 export default function App() {
+  const location = useLocation();
   return (
-    <div className="grid grid-cols-[350px,1fr,300px] h-screen overflow-hidden">
+    <div className="grid grid-cols-[450px,1fr] h-screen overflow-hidden">
       <div>
         <SideBar />
       </div>
       <div>
+        {location.pathname === "/" ? (
+          <div className="h-full w-full flex items-center justify-center">
+            <p className="font-semibold text-xl">Select the chat</p>
+          </div>
+        ) : null}
         <Outlet />
       </div>
-      <div></div>
     </div>
   );
 }
