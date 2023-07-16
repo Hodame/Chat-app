ea161b4e0faaee0bbc982ba21fb3aee8a04e806d
hodame <hodame04@gmail.com>
2023-07-16 23:15:43 +0500

    change app theme

---
+++ src/theme/theme.tsx
@@ -0,0 +1,68 @@
+import { defineStyleConfig, extendTheme } from '@chakra-ui/react';
+
+const styles = {
+  global: {
+    body: {
+      bg: 'background',
+      _dark: {
+        bg: 'dark'
+      }
+    }
+  }
+};
+
+const colors = {
+  colors: {
+    background: '#202329',
+    dark: '#131313',
+    surface: '#2E333D',
+    primary: {
+      main: '#6b8afd',
+      50: '#edf4ff',
+      100: '#dfeaff',
+      200: '#c5d8ff',
+      300: '#a1bcff',
+      400: '#6b8afd',
+      500: '#5d72f7',
+      600: '#3f4aec',
+      700: '#3239d0',
+      800: '#2b32a8',
+      900: '#2a3185'
+    }
+  }
+};
+
+const components = {
+  Spinner: defineStyleConfig({
+    baseStyle: {
+      color: 'primary.400'
+    }
+  }),
+  Button: defineStyleConfig({
+    variants: {
+      solid: {
+        rounded: '3xl',
+        background: 'primary.400',
+        _hover: {
+          background: 'primary.500'
+        },
+        _active: {
+          background: 'primary.400'
+        }
+      }
+    }
+  })
+};
+
+export const theme = extendTheme(
+  colors,
+  {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+    fonts: {
+      body: `'Montserrat', sans-serif`
+    },
+    styles,
+    components
+  }
+);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,17 +5,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'primary': '#ef233c',
-      'primary-dark': '#d90429',
-      'light': '#edf2f4',
-      'surface': '#8d99ae',
-      'surface-dark': '#2b2d42'
-    },
     fontFamily: {
       sans: ['"PT Sans"', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      animation: {
+        'message-appear': 'message-appear 0.3s ease-in-out '
+      },
+      keyframes: {
+        'message-appear': {
+          '0%': { transform: 'scale(0.95)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        }
+      },
+      colors: {
+        'primary': '#6B8AFD',
+        'dark': '#131313',
+        'background': '#202329',
+        'surface': '#2E333D',
+        'light': '#B0B5C1 '
+      },
+    },
   },
   plugins: [],
 }
