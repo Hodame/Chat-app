a940c61e75e6cee2cb3dd75bff41d577c735a233
hodame <hodame04@gmail.com>
2023-07-18 17:50:49 +0500

    create hook for check user auth

--- src/helpers/useFirebaseAuth.ts
+++ src/helpers/useFirebaseAuth.ts
@@ -1,25 +1,64 @@
 import { useEffect, useState } from "react"
 import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "@/firebase/config"
+import { auth, db } from "@/firebase/config"
+import useUserStore, { User } from "@/store/userStore";
+import { child, get, ref } from "firebase/database";
 
-const useFirebaseAuth = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false)
+export const useAuthListener = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isCheckingStatus, setCheckingStatus] = useState(true);
+  const [userId, setUserId] = useState<string | null>(null)
+
+  const writeUser = useUserStore(state => state.writeUser)
 
   useEffect(() => {
-    const listen = onAuthStateChanged(auth, (user) => {
+    const getUser = async () => {
+      try {
+        await get(child(ref(db), 'users/' + userId)).then((user) => {
+          if (user.exists()) {
+            const userData = user.val() as User
+            writeUser(userData)
+            return
+          }
+          writeUser({
+            username: "",
+            userID: "",
+            email: "",
+            photoURL: null,
+            chats: null
+          })
+        })
+      } catch (error) {
+        alert(error)
+      }
+    }
+
+    if (userId) {
+      getUser()
+      return
+    }
+
+    writeUser({
+      username: "",
+      userID: "",
+      email: "",
+      photoURL: null,
+      chats: null
+    })
+  }, [userId])
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true)
-      } else {
-        setLoggedIn(false)
+        setUserId(user.uid)
+        setCheckingStatus(false);
+        return
       }
-    })
+      setUserId(null)
+      setCheckingStatus(false);
+    });
+  }, []);
 
-    return () => {
-      listen()
-    }
-  }, [])
-
-  return isLoggedIn
-}
-
-export default useFirebaseAuth
+  return { isLoggedIn, isCheckingStatus, userId };
+};
