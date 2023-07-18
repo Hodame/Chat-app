6e1ed3b191f68332355c14cb76e916215da5807d
hodame <hodame04@gmail.com>
2023-07-18 17:51:17 +0500

    refactoring

--- src/components/ChatPage/ChatTile.tsx
+++ src/components/ChatPage/ChatTile.tsx
@@ -1,24 +1,17 @@
 import { Avatar } from "@chakra-ui/react";
 
-type ChatTileProps = {
-  avatarLink: string;
-  userName: string;
-  lastMessage: string;
-  messageTime: string;
-  isRead?: boolean;
-  messageCount: number
-};
+type ChatTileProps = ChatTile
 
-export default function ChatTile({ avatarLink, userName, lastMessage, messageTime, isRead, messageCount}: ChatTileProps) {
+export default function ChatTile({ photoURL, username, lastMessage, messageTime, isRead, messageCount}: ChatTileProps) {
   return (
     <div className="px-3 py-3 rounded-3xl flex justify-between cursor-pointer hover:bg-surface">
       <Avatar
-        name={userName}
+        name={username}
         size="lg"
-        src={avatarLink}
+        src={photoURL}
       />
       <div className=" mx-2 flex-auto">
-        <h1 className="text-white font-medium">{userName}</h1>
+        <h1 className="text-white font-medium">{username}</h1>
         <p className="line-clamp-2 text-sm text-light">
           {lastMessage}
         </p>
