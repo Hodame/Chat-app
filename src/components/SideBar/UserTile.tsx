7e67951b9212817c06d2e21af0e3d029f3f72d42
hodame <hodame04@gmail.com>
2023-07-18 17:53:27 +0500

    create user tile for search user preview

---
+++ src/components/SideBar/UserTile.tsx
@@ -0,0 +1,21 @@
+import TimeAgo from 'timeago-react';
+
+import { Avatar } from "@chakra-ui/react"
+
+type UserTileProps = {
+  photoURL: string
+  username: string
+  lastOnline: string
+}
+
+export default function UserTile({ photoURL, username, lastOnline }: UserTileProps) {
+  return (
+    <div className="flex gap-3 items-center rounded-3xl p-3 hover:bg-surface">
+      <Avatar size={'lg'} name={username} src={photoURL}/>
+      <div>
+        <h1 className="text-xl font-semibold">{username}</h1>
+        <TimeAgo className="text-light text-sm font-medium" live datetime={lastOnline}/>
+      </div>
+    </div>
+  )
+}
