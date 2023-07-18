8f55baf31f05cd4a11a75232080cfc0a0a700893
hodame <hodame04@gmail.com>
2023-07-18 17:51:39 +0500

    create route guard for main layout

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,30 +1,38 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom"
 
+import { useAuthListener } from "@/helpers/useFirebaseAuth"
+import { Spinner } from "@chakra-ui/react"
+
 import SideBar from "../components/SideBar/SideBar"
-import useFirebaseAuth from "@/helpers/useFirebaseAuth"
+import { useEffect } from "react"
 
 export default function Main() {
   const location = useLocation()
-  const isUser = useFirebaseAuth()
-  console.log(isUser)
-
-  if (!isUser) {
-    return <Navigate to={"auth"} />
-  }
+  const { isLoggedIn, isCheckingStatus } = useAuthListener()
 
   return (
-    <div className="grid grid-cols-[450px,1fr] h-screen overflow-hidden">
-      <div>
-        <SideBar />
-      </div>
-      <div>
-        {location.pathname === "/" ? (
-          <div className="h-full w-full flex items-center justify-center">
-            <p className="font-semibold text-xl">Select the chat</p>
+    <>
+      {isCheckingStatus ? (
+        <div className="w-full h-screen flex items-center justify-center">
+          <Spinner size={"xl"} />
+        </div>
+      ) : isLoggedIn ? (
+        <div className="grid grid-cols-[450px,1fr] h-screen overflow-hidden">
+          <div>
+            <SideBar />
           </div>
-        ) : null}
-        <Outlet />
-      </div>
-    </div>
+          <div>
+            {location.pathname === "/" ? (
+              <div className="h-full w-full flex items-center justify-center">
+                <p className="font-semibold text-xl">Select the chat</p>
+              </div>
+            ) : null}
+            <Outlet />
+          </div>
+        </div>
+      ) : (
+        <Navigate to="/auth" />
+      )}
+    </>
   )
 }
