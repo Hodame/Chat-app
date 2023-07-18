59599eb881eb1c0e13a1f719eee77a96e26d0994
hodame <hodame04@gmail.com>
2023-07-18 17:49:45 +0500

    add router in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { Route, Routes } from "react-router-dom"
-import Main from "./containers/Main"
-import ProtectedRoute from "./router/ProtectedRoute"
-import Auth from "./containers/Auth"
+import RoutesRouter from "./router/routes"
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/" element={<Main />} />
-      </Routes>
+      <RoutesRouter/>
     </>
   )
 }
+  
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import "./index.css"
 
 import { ChakraBaseProvider, ColorModeScript } from "@chakra-ui/react"
 import { theme } from "./theme/theme.tsx"
-import { BrowserRouter } from "react-router-dom"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
@@ -13,10 +12,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
       <ColorModeScript initialColorMode={theme.initialColorMode} />
       <ChakraBaseProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </ChakraBaseProvider>
-    </React.StrictMode>
+    </React.StrictMode> 
   </>
 )
