2ef1e5d6c0220efc64ca316726b3e7ad7673e203
hodame <hodame04@gmail.com>
2023-07-19 17:43:17 +0500

    add serach user functional and other few things

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,15 +1,16 @@
 import { Button } from "@chakra-ui/react"
-import { HiCog } from "react-icons/hi"
+import { HiArrowSmLeft, HiCog } from "react-icons/hi"
 import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 
 import SearchBar from "@/components/SideBar/SearchBar"
 import ChatTile from "@/components/ChatPage/ChatTile"
-import SideBarSelect from "./SideBarSelect"
-import { equalTo, onValue, orderByChild, query, ref } from "firebase/database"
 import { db } from "@/firebase/config"
+
+import SideBarSelect from "./SideBarSelect"
 import useUserStore from "@/store/userStore"
-import UserTile from "./UserTile"
+import UserTile, { UserTileProps } from "./UserTile"
+import { collection, getDocs, onSnapshot, query, where } from "firebase/firestore"
 
 export default function SideBar() {
   const user = useUserStore((state) => state.user)
@@ -17,13 +18,14 @@ export default function SideBar() {
   const [isSearch, setIsSearch] = useState(false)
   const [searchWord, setSearchWord] = useState("")
   const [chats, setChats] = useState<ChatTile[] | null>(null)
+  const [searchResult, setSearchResult] = useState<UserTileProps[] | null>(null)
 
   async function getUserChats() {
     try {
-      const chatQuery = query(ref(db, "chats"), orderByChild("members"), equalTo(user.userID))
-      await onValue(chatQuery, (snapshot) => {
-        if (snapshot.exists()) {
-          const data = snapshot.val() as ChatTile[]
+      const chatQuery = query(collection(db, "chats"), where("members", "array-contains", user.userID))
+      await onSnapshot(chatQuery, (snapshot) => {
+        if (!snapshot.empty) {
+          const data = snapshot.docs.map((doc) => doc.data()) as ChatTile[]
           setChats(data)
         }
       })
@@ -32,8 +34,27 @@ export default function SideBar() {
     }
   }
 
-  useEffect(() => {
+  const toggleSearchBar = () => (setIsSearch(false), setSearchWord(""))
 
+  useEffect(() => {
+    async function getSeachUsers() {
+      try {
+        const userQuery = query(collection(db, "users"), where("username", "==", searchWord))
+        await getDocs(userQuery).then((result) => {
+          const data = result.docs.map((doc) => doc.data()) as UserTileProps[]
+          console.log(searchWord)
+          if (!result.empty) {
+            setSearchResult(data)
+          }
+        })
+      } catch (error) {}
+    }
+
+    if (searchWord.length) {
+      getSeachUsers()
+    } else {
+      setSearchResult(null)
+    }
   }, [searchWord])
 
   useEffect(() => {
@@ -44,35 +65,49 @@ export default function SideBar() {
     <div className="flex w-full flex-col h-full mr-2 overflow-auto bg-background">
       <div className="px-4 pt-4">
         <div className="my-4 px-4 flex items-center gap-2">
-          <SideBarSelect
-            button={
-              <Button h={"56px"} w={"56px"} background={"primary.400"} rounded={"3xl"}>
+          {isSearch ? (
+            <div>
+              <Button onClick={toggleSearchBar} h={"56px"} w={"56px"}>
                 <div className="text-3xl text-white">
-                  <HiCog />
+                  <HiArrowSmLeft />
                 </div>
               </Button>
-            }
-          />
+            </div>
+          ) : (
+            <SideBarSelect
+              button={
+                <Button h={"56px"} w={"56px"} rounded={"3xl"}>
+                  <div className="text-3xl text-white">
+                    <HiCog />
+                  </div>
+                </Button>
+              }
+            />
+          )}
           <div className="flex-auto">
-            <SearchBar onSearch={(e) => setSearchWord(e.target.value)} hintText="Search for dialog" />
+            <SearchBar
+              value={searchWord}
+              onFocus={() => setIsSearch(true)}
+              onSearch={(e) => setSearchWord(e.target.value)}
+              hintText="Search for dialog"
+            />
           </div>
         </div>
-        {searchWord}
         {isSearch ? (
-          <SearchList />
+          <SearchList users={searchResult} />
         ) : user.chats ? (
           chats ? (
             <ChatList chats={chats} />
           ) : (
             <>
-              <div className="h-full flex items-center justify-center">
+              <div className="screen-center">
                 <h1 className="font-semibold">Somethins went wrong...</h1>
               </div>
             </>
           )
         ) : (
           <>
-            <div className="h-full flex items-center justify-center">
+            <div className="screen-center">
               <h1 className="font-semibold">Dont have chats? Find someone and start chating!</h1>
             </div>
           </>
@@ -105,10 +140,24 @@ function ChatList({ chats }: ChatListProps) {
   )
 }
 
-function SearchList() {
+function SearchList({ users }: { users: UserTileProps[] | null }) {
   return (
-    <div>
-      <UserTile lastOnline={"December 17, 1995 03:24:00"} photoURL="bebra" username="bibra" />
-    </div>
+    <>
+      {users ? (
+        <div>
+          {users.map((user) => {
+            return (
+              <Link to={"/" + user.userID}>
+                <UserTile lastOnline={"December 17, 1995 03:24:00"} photoURL={user.photoURL} username={user.username} />
+              </Link>
+            )
+          })}
+        </div>
+      ) : (
+        <div className="screen-center">
+          <p className="font-semibold">Sorry we couldn't find someone...</p>
+        </div>
+      )}
+    </>
   )
 }
