dd3805bc2393a66ed6a3581eedc5ba6eeb0b5019
hodame <hodame04@gmail.com>
2023-07-19 17:42:31 +0500

    add props for search bar

--- src/components/SideBar/SearchBar.tsx
+++ src/components/SideBar/SearchBar.tsx
@@ -1,17 +1,21 @@
 import { Icon } from "@chakra-ui/react"
-import { ChangeEventHandler } from "react"
+import { ChangeEventHandler, FocusEventHandler } from "react"
 import { HiSearch } from "react-icons/hi"
 
 type SearchBarProps = {
+  value: string
   hintText?: string
   onSearch?: ChangeEventHandler<HTMLInputElement>
+  onFocus?: FocusEventHandler<HTMLInputElement>
 }
 
-export default function SearchBar({ hintText, onSearch }: SearchBarProps) {
+export default function SearchBar({ value, hintText, onSearch, onFocus }: SearchBarProps) {
   return (
     <div className="bg-surface rounded-3xl relative text-light">
       <Icon className="absolute left-4 top-[50%] -translate-y-[50%]" w={7} h={7} as={HiSearch} />
       <input
+        value={value}
+        onFocus={onFocus}
         onChange={onSearch}
         className="bg-transparent w-full px-3 py-5 pl-14 text-white rounded-3xl"
         placeholder={hintText}
