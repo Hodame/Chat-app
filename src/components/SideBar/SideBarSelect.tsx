e51f58ef7f001ecb84dd4ce7f216b140a30364b8
hodame <hodame04@gmail.com>
2023-07-20 17:35:09 +0500

    refactoring

--- src/components/SideBar/SideBarSelect.tsx
+++ src/components/SideBar/SideBarSelect.tsx
@@ -1,5 +1,5 @@
 import { auth } from "@/firebase/config"
-import { Icon, Menu, MenuButton, MenuItem, MenuList, Avatar } from "@chakra-ui/react"
+import { Icon, Menu, MenuButton, MenuItem, MenuList, Avatar, Button } from "@chakra-ui/react"
 import { signOut } from "firebase/auth"
 import { HiCog, HiHashtag, HiLogout } from "react-icons/hi"
 import { useNavigate } from "react-router-dom"
@@ -12,7 +12,7 @@ type SideBarSelectProps = {
 
 export default function SideBarSelect({ button }: SideBarSelectProps) {
   const navigate = useNavigate()
-  const user = useUserStore(state => state.user)
+  const user = useUserStore((state) => state.user)
 
   async function loginOut() {
     try {
@@ -26,22 +26,22 @@ export default function SideBarSelect({ button }: SideBarSelectProps) {
   return (
     <div>
       <Menu>
-        <MenuButton>{button}</MenuButton>
+        <MenuButton h={"56px"} w={"56px"} rounded={'3xl'} as={Button} variant={'solid'}>{button}</MenuButton>
         <MenuList>
           <MenuItem>
-            <Avatar size={'xs'} name={user.username} src={user?.photoURL ?? undefined} className="mr-3" />
+            <Avatar size={"xs"} name={user.username} src={user?.photoURL ?? undefined} className="mr-3" />
             <span>{user.username}</span>
           </MenuItem>
           <MenuItem>
-            <Icon fontSize={'2xl'} as={HiCog} className="mr-3" />
+            <Icon fontSize={"2xl"} as={HiCog} className="mr-3" />
             <span>Settings</span>
           </MenuItem>
           <MenuItem>
-            <Icon fontSize={'2xl'} as={HiHashtag} className="mr-3" />
+            <Icon fontSize={"2xl"} as={HiHashtag} className="mr-3" />
             <span>FAQ</span>
           </MenuItem>
           <MenuItem onClick={loginOut}>
-            <Icon fontSize={'2xl'} as={HiLogout} className="mr-3" />
+            <Icon fontSize={"2xl"} as={HiLogout} className="mr-3" />
             <span>Log out</span>
           </MenuItem>
         </MenuList>
