70621e48edae30c57be32616220372fa0d83e847
hodame <hodame04@gmail.com>
2023-07-20 17:35:31 +0500

    finished resize text area component now its perfect

--- src/components/UI/ResizeTextArea.tsx
+++ src/components/UI/ResizeTextArea.tsx
@@ -1,10 +1,13 @@
-import { ChangeEvent, useRef } from "react"
+import { ChangeEvent, ChangeEventHandler, KeyboardEventHandler, forwardRef, useImperativeHandle, useRef } from "react"
 
 type ResizeTextAreaProps = {
   placeholder: string
+  value: string
+  onChange: ChangeEventHandler<HTMLTextAreaElement>
+  onKeyPress: KeyboardEventHandler<HTMLTextAreaElement>
 }
 
-export default function ResizeTextArea({ placeholder }: ResizeTextAreaProps) {
+const ResizeTextArea = forwardRef(({ placeholder, value, onChange, onKeyPress }: ResizeTextAreaProps, ref) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null)
 
   function resize(e: ChangeEvent<HTMLTextAreaElement>) {
@@ -12,17 +15,27 @@ export default function ResizeTextArea({ placeholder }: ResizeTextAreaProps) {
     textareaRef.current.style.height = "auto"
     textareaRef.current.style.height = e.target.scrollHeight + "px"
   }
-  
+
+  useImperativeHandle(ref, () => ({
+    setAuto() {
+      if (!textareaRef.current) return
+      textareaRef.current.style.height = "auto"
+    }
+  }))
+
   return (
     <>
       <textarea
         ref={textareaRef}
-        onChange={resize}
-        onInput={() => console.log("bebra")}
+        onKeyDown={(e) => onKeyPress(e)}
+        value={value}
+        onChange={(e) => (resize(e), onChange(e))}
         rows={1}
         placeholder={placeholder}
         className="bg-transparent py-3 px-12 w-full rounded-3xl max-h-72 resize-none"
       />
     </>
   )
-}
+})
+
+export default ResizeTextArea
