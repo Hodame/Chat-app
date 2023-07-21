0d83f5073272b53aba66ea19485d1a8f3e19d5f0
hodame <hodame04@gmail.com>
2023-07-21 15:42:12 +0500

    add ref for messages list

--- src/components/ChatPage/MessagesList.tsx
+++ src/components/ChatPage/MessagesList.tsx
@@ -5,7 +5,7 @@ import MessageLine from "./MessageLine"
 
 //@ts-ignore
 import dateFormat from "dateformat"
-import { forwardRef } from "react"
+import { forwardRef, useImperativeHandle, useRef } from "react"
 
 type MessagesListProps = {
   messages: Message[] | null
@@ -13,19 +13,22 @@ type MessagesListProps = {
 }
 
 const MessagesList = forwardRef<HTMLDivElement, MessagesListProps>(({ messages, reciver }, ref) => {
+  const messsageRef = useRef<HTMLDivElement | null>(null)
   const user = useUserStore((state) => state.user)
+
+  useImperativeHandle(ref, () => messsageRef.current!)
 
   return (
     <>
       {messages ? (
-        <div ref={ref}>
+        <div ref={messsageRef}>
           {messages.map((message, idx) => (
             <MessageLine
               key={idx}
               message={message.message}
               userName={message.sentBy.username}
               fromUser={user.userID === message.sentBy.userID}
-              timeSend={dateFormat(new Date(message.sentAt.seconds), "HH:MM")}
+              timeSend={dateFormat(new Date(message.sentAt.toDate()), "HH:MM")}
             />
           ))}
           <span></span>
