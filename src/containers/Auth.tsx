d6708f977e7ef9e14a5a76f037089d0e45170216
hodame <hodame04@gmail.com>
2023-07-21 15:42:41 +0500

    improve code

--- src/containers/Auth.tsx
+++ src/containers/Auth.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 import { useAuthListener } from "@/helpers/useFirebaseAuth"
-import { Spinner } from "@chakra-ui/react"
+import { Button, Spinner } from "@chakra-ui/react"
 import { Navigate } from "react-router-dom"
 
 import Login from "./Login"
@@ -9,10 +9,10 @@ import Register from "./Register"
 
 export default function Auth() {
   const { isLoggedIn, isCheckingStatus } = useAuthListener()
-  const [isLogin, setLogin] = useState(true)
+  const [isLogin, setLogin] = useState(false)
 
   return (
-    <>
+    <div>
       {isCheckingStatus ? (
         <div className="w-full h-screen flex items-center justify-center">
           <Spinner size={"xl"} />
@@ -25,9 +25,14 @@ export default function Auth() {
             <h1 className="font-semibold text-4xl">MessengerX</h1>
             <p className="font-medium text-xl text-light">Join and discover a new world of communication</p>
           </div>
-          {isLogin ? <Login goRegister={() => setLogin(false)} /> : <Register goLogin={() => setLogin(true)} />}
+          {isLogin ? <Login /> : <Register />}
+          <div className="mt-3 flex justify-center">
+            <Button onClick={() => setLogin((value) => (value = !value))} rounded={"full"} variant={"ghost"}>
+              <p>{isLogin ? "Register" : "Login"}</p>
+            </Button>
+          </div>
         </div>
       )}
-    </>
+    </div>
   )
 }
