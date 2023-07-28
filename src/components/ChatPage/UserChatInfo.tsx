a8777b04f0f18ef5048d3c430f41d58277ba5825
hodame <hodame04@gmail.com>
2023-07-28 17:39:22 +0500

    and live online status and last seen timer

--- src/components/ChatPage/UserChatInfo.tsx
+++ src/components/ChatPage/UserChatInfo.tsx
@@ -1,7 +1,10 @@
-import { MouseEventHandler } from "react"
+import { MouseEventHandler, useContext } from "react"
 import { HiDotsHorizontal, HiSearch } from "react-icons/hi"
 import { Avatar, AvatarBadge, Button } from "@chakra-ui/react"
+import { ReciverContext } from "@/context/ReciverContext"
+
 import ChatSelectMenu from "./ChatSelectMenu"
+import TimeAgo from "timeago-react"
 
 type UserChatInfoProps = {
   avatar?: string | null
@@ -10,15 +13,21 @@ type UserChatInfoProps = {
 }
 
 export default function UserChatInfo({ avatar, name, searchEvent }: UserChatInfoProps) {
+  const reciver = useContext(ReciverContext)
   return (
     <div className="flex items-center p-3 justify-between w-full">
       <div className="flex items-center">
         <Avatar className="mr-3" name={name} src={avatar ?? undefined}>
-          <AvatarBadge boxSize="1.0em" bg="green.500" />
+          {reciver.isOnline ? <AvatarBadge boxSize="1.0em" bg="green.500" /> : null}
         </Avatar>
         <div>
           <h1 className="text-lg font-semibold">{name}</h1>
-          <p className="text-primary">Online</p>
+          {!reciver.isOnline && reciver.lastOnline ? (
+            <div className="text-light font-bold text-sm">
+              Last seen <TimeAgo opts={{ minInterval: 1 }} datetime={reciver.lastOnline.toDate()} />
+            </div>
+          ) : null}
+          {reciver.isOnline ? <p className="text-primary">Online</p> : null}
         </div>
       </div>
       <div className="flex gap-2">
