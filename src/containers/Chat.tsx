dae961677a991bd0013fb605dc5516846bef2790
hodame <hodame04@gmail.com>
2023-07-28 17:40:26 +0500

    refactoring

--- src/containers/Chat.tsx
+++ src/containers/Chat.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from "@chakra-ui/react"
 import { useLocation, useParams } from "react-router-dom"
 import { useEffect, useRef, useState } from "react"
-import { collection, doc, getDoc, onSnapshot } from "firebase/firestore"
+import { collection, doc, getDoc, onSnapshot, query, where } from "firebase/firestore"
 import { Message } from "@/types/chat"
 import { db } from "@/firebase/config"
 import { ReciverContext } from "@/context/ReciverContext"
@@ -15,23 +15,33 @@ import UserChatInfo from "@/components/ChatPage/UserChatInfo"
 import MessagesList from "@/components/ChatPage/MessagesList"
 import ChatInput from "@/components/ChatPage/ChatInput"
 
+const initialReciverValue: User = {
+  username: "",
+  email: "",
+  userID: "",
+  photoURL: null,
+  isOnline: false,
+  lastOnline: null,
+}
+
 export default function Chat() {
   const location = useLocation()
   const messagesRef = useRef<HTMLDivElement | null>(null)
   const user = useUserStore((state) => state.user)
   const { chatID } = useParams()
 
-  const [reciver, setChat] = useState<User | null>(null)
+  const [reciver, setReciver] = useState<User | null>(null)
   const [messages, setMessages] = useState<Message[] | null>(null)
   const [isLoading, setIsLoading] = useState(true)
   const [isSearchBar, setSearchBar] = useState(false)
 
   async function getReciverInfo() {
     try {
-      await getDoc(doc(db, "users", chatID!)).then((doc) => {
-        if (doc.exists()) {
-          const data = doc.data() as User
-          setChat(data)
+      const reciverRef = doc(db, "users", chatID!)
+      onSnapshot(reciverRef, (snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.data() as User
+          setReciver(data)
         }
       })
     } catch (error) {
@@ -46,6 +56,7 @@ export default function Chat() {
         if (!snapshot.empty) {
           const data = snapshot.docs.map((doc) => doc.data()) as Message[]
           const sorted = data.sort((message, message2) => message.sentAt.seconds - message2.sentAt.seconds)
+
           setMessages(sorted)
         }
       })
@@ -56,23 +67,26 @@ export default function Chat() {
 
   useEffect(
     function () {
-      const wait = async () => {
-        await Promise.all([getReciverInfo(), getMessages()])
-        setIsLoading(false)
-        setTimeout(() => {
-          messagesRef.current?.scrollIntoView({ behavior: "instant", block: "end" })
-        }, 1)
-      }
-      wait()
+      getReciverInfo()
+      getMessages()
       return () => {
-        setIsLoading(true), setSearchBar(false)
+        setIsLoading(true)
+        setSearchBar(false)
+        setMessages(null)
       }
     },
     [location.pathname]
   )
 
+  useEffect(() => {
+    reciver || messages ? setIsLoading(false) : null
+    setTimeout(() => {
+      messagesRef.current?.scrollIntoView({ behavior: "instant", block: "end" })
+    }, 1)
+  }, [reciver, messages])
+
   return (
-    <ReciverContext.Provider value={reciver ?? { username: "", email: "", userID: "", photoURL: null }}>
+    <ReciverContext.Provider value={reciver ?? initialReciverValue}>
       <div className={"grid " + (isSearchBar ? "grid-cols-[1fr,0.5fr]" : "grid-cols-[1fr]")}>
         {isLoading ? (
           <div className="h-screen">
@@ -99,7 +113,7 @@ export default function Chat() {
               </div>
             </div>
             <div>
-              <ChatInput messagesRef={messagesRef} reciver={reciver} />
+              <ChatInput reciver={reciver} />
             </div>
           </div>
         )}
